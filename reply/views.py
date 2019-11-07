31fc70b13045b0b8fa217bcb4f1f3f9f3ca56c59
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-11-07 17:45:24 +0000

    Added form submission logic to rsvp view

--- reply/views.py
+++ reply/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
 from .forms import DetailForm
 
 
@@ -26,9 +28,35 @@ def gifts(request):
 
 def rsvp(request):
     '''
-    Renders rsvp page
+    Renders rsvp page and allows users to reply
+    to the invitation by entering their details
     '''
-    detail_form = DetailForm()
+    if request.method == "POST":
+        detail_form = DetailForm(request.POST)
+        # Get the guest_name value from the form
+        guest_name = request.POST.get("guest_name")
+        # Get the attending value from the form
+        attending = request.POST.get("attending")
+        if detail_form.is_valid():
+            # Ensure the guest_name or attending values are valid options
+            if guest_name != "Choose an option..."\
+              or attending != "Choose an option...":
+                detail_form.save()
+                # If the user selects 'No' from the attending options
+                if attending == "No":
+                    messages.success(request, f"Sorry you can't make it :(")
+                # If the user doesn't select 'No' from the attending options
+                else:
+                    messages.success(request, f"Great, see you there :)")
+                return redirect(home)
+            # If the attending values aren't valid options
+            else:
+                messages.error(request,
+                               "Please select a valid dropdown option")
+                return redirect(home)
+
+    else:
+        detail_form = DetailForm()
 
     context = {
         'detail_form': detail_form,
