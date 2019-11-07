8d607292930249d8ff43c0aaba30d0c121b522cf
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-11-07 12:38:33 +0000

    Added Guest model to reply app

--- reply/models.py
+++ reply/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Guest(models.Model):
+    '''
+    Provides the choices for the guest names
+    '''
+    GUEST_CHOICES = (
+        ("Ash and Saf", "Ash and Saf"),
+        ("Becky and Stu", "Becky and Stu"),
+        ("Brittany and Jake", "Brittany and Jake"),
+        ("Bunty", "Bunty"),
+        ("Charlotte and Sean", "Charlotte and Sean"),
+        ("Gary, Vicki and Callum", "Gary, Vicki and Callum"),
+        ("Giri and Gill", "Giri and Gill"),
+        ("Indira", "Indira"),
+        ("Jack (Best Man)", "Jack (Best Man)"),
+        ("Jack, Toni and Archie", "Jack, Toni and Archie"),
+        ("Jimmy and Iris", "Jimmy and Iris"),
+        ("Josh, Amy, Oscar and Archie", "Josh, Amy, Oscar and Archie"),
+        ("Lee and Will", "Lee and Will"),
+        ("Lisa and David", "Lisa and David"),
+        ("Paul and Tracey", "Paul and Tracey"),
+        ("Rach, Ste, Bobby and Sam", "Rach, Ste, Bobby and Sam"),
+    )
+
+    guest_name = models.CharField(
+        max_length=27,
+        unique=True,
+        choices=GUEST_CHOICES)
+
+    def __str__(self):
+        return self.guest_name
