fead1037140ab5e603cf029fca2dc4db795b21ab
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-11-10 11:05:13 +0000

    Added reply/admin url to reply urls.py

--- reply/urls.py
+++ reply/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('location', views.location, name="location"),
     path('gifts', views.gifts, name="gifts"),
     path('rsvp', views.rsvp, name="rsvp"),
+    path('admin', views.admin, name="admin"),
 ]
