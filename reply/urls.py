bc17e2d5f77513ce658af8b27c71294c2d25f294
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 20:08:09 +0100

    Removed gifts url from base.html and urls.py

--- reply/urls.py
+++ reply/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('location', views.location, name="location"),
-    path('gifts', views.gifts, name="gifts"),
     path('rsvp', views.rsvp, name="rsvp"),
     path('admin', views.admin, name="admin"),
 ]
