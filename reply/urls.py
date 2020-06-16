a2b477d05c71e48993149576aa2bf5a06be99adc
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 21:44:14 +0100

    Created url for RSVPViewSet

--- reply/urls.py
+++ reply/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.rsvp('rsvp', views.RSVPViewSet)
+
 urlpatterns = [
+    path('rsvp', include(router.urls)),
     path('location', views.location, name="location"),
     path('rsvp', views.rsvp, name="rsvp"),
     path('admin', views.admin, name="admin"),
